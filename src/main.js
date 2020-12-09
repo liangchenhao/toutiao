75e58abd2daaf8ca6dad55809da2b14d45c54486
liangchenhao <376734498@qq.com>
2020-12-09 21:31:55 +0800

    项目初始化调整目录和引入

--- src/main.js
+++ src/main.js
@@ -2,6 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import './styles/index.less'
+// 加载vant核心组件
+import Vant from 'vant'
+// 加载vant全局样式
+import 'vant/lib/index.css'
+// 注册使用vant组件库
+Vue.use(Vant)
 
 Vue.config.productionTip = false
 
